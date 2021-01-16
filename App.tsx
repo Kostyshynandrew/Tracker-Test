7fd843a60c3b053de53db3ca2147ab0cd1b03d97
Andrii Kostyshyn <a.kostyshyn@apiko.com>
2021-01-16 13:20:25 +0200

    Add stateManager + navigation

--- App.tsx
+++ App.tsx
@@ -1,13 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
+    <NavigationContainer>
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
+    </NavigationContainer>
   );
 }
 
